0f240791eb0cd71bb9dccf5f4ae2c88958361573
kowalix007 L.K <kowalix007@gmail.com>
2020-06-22 09:40:59 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,12 +18,18 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
+        :name => "Lord Capulet"
+        :age => "50"
         #your key/value pairs here
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
+        :name => "Juliet",
+        :age => "15"
+        :status => "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
